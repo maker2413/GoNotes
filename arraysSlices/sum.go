2a97cd46a87d3896aa20b703062aba724a2d8267
maker2413 <maker2413@gmail.com>
2024-10-01 17:15:07 -0500

    worked through arrays and slices again

--- arraysSlices/sum.go
+++ arraysSlices/sum.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-func Sum(numbers [5]int) int {
-	var sum int
+func Sum(numbers []int) int {
+	sum := 0
 	// range lets you iterate over an array. On each iteration,
 	// range returns two values - the index and the value.
 	// We are choosing to ignore the index value by using _ blank identifier.
@@ -14,8 +14,49 @@ func Sum(numbers [5]int) int {
 	return sum
 }
 
+// https://gobyexample.com/variadic-functions
+func SumAll(numbersToSum ...[]int) []int {
+	// There's a new way to create a slice. make allows you to create a slice with a
+	// starting capacity of the len of the numbersToSum we need to work through. The
+	// length of a slice is the number of elements it holds len(mySlice), while the
+	// capacity is the number of elements it can hold in the underlying array cap(mySlice),
+	// e.g., make([]int, 0, 5) creates a slice with length 0 and capacity 5.
+	// example: sums := make([]int, len(numbersToSum))
+
+	// As mentioned, slices have a capacity. If you have a slice with a capacity of 2 and
+	// try to do mySlice[10] = 1 you will get a runtime error.
+	// However, you can use the append function which takes a slice and a new value, then
+	// returns a new slice with all the items in it.
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			// Slices can be sliced! The syntax is slice[low:high]. If you omit the value
+			// on one of the sides of the : it captures everything to that side of it. In
+			// our case, we are saying "take from 1 to the end" with numbers[1:]. You may
+			// wish to spend some time writing other tests around slices and experiment
+			// with the slice operator to get more familiar with it.
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
 func main() {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
 	fmt.Println(Sum(numbers))
 }
--- arraysSlices/sum_test.go
+++ arraysSlices/sum_test.go
@@ -1,19 +1,65 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3}
 
 	got := Sum(numbers)
-	want := 15
-	assertCorrectAnswer(t, got, want, numbers)
-}
+	want := 6
 
-func assertCorrectAnswer(t testing.TB, got, want int, numbers [5]int) {
-	t.Helper()
 	if got != want {
 		// %v is used to print the "default" format, which works well for arrays
 		t.Errorf("expected: '%d', but got: '%d', input: %v", want, got, numbers)
 	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// The reflect package is used to reflect data between data types.
+	// The DeepEqual function will compare the values within our slices in this case.
+	// NOTE: It's important to note that reflect.DeepEqual is not "type safe" - the code
+	// will compile even if you did something a bit silly.
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("expected: '%v', but got: '%v'", want, got)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	// We could've created a new function checkSums like we normally do, but in this
+	// case, we're showing a new technique, assigning a function to a variable. It might
+	// look strange but, it's no different to assigning a variable to a string, or an
+	// int, functions in effect are values too.
+
+	// It's not shown here, but this technique can be useful when you want to bind a
+	// function to other local variables in "scope" (e.g between some {}). It also allows
+	// you to reduce the surface area of your API.
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected: '%v', but got: '%v'", want, got)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9})
+		want := []int{5, 9}
+		// The reflect package is used to reflect data between data types.
+		// The DeepEqual function will compare the values within our slices in this case.
+		// NOTE: It's important to note that reflect.DeepEqual is not "type safe" - the code
+		// will compile even if you did something a bit silly.
+
+		checkSums(t, got, want)
+	})
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
 }
