a1d4888e3c426944c061a3a1197882b08cae0f6a
maker2413 <maker2413@gmail.com>
2024-09-30 20:46:47 -0500

    working through learning go with tests again

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -2,32 +2,10 @@ package main
 
 import "fmt"
 
-const french = "French"
-const spanish = "Spanish"
-const englishHelloPrefix = "Hello, "
-const frenchHelloPrefix = "Bonjour, "
-const spanishHelloPrefix = "Hola, "
-
-func Hello(name string, language string) string {
-	if name == "" {
-		name = "World"
-	}
-
-	return greetingPrefix(language) + name
-}
-
-func greetingPrefix(language string) (prefix string) {
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	default:
-		prefix = englishHelloPrefix
-	}
-	return
+func Hello(name string) string {
+	return "Hello, " + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("world", ""))
+	fmt.Println(Hello("world"))
 }
