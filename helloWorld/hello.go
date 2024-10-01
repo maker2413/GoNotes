1943ce4b655c5b901b088cd7a04bd428f7a3a035
maker2413 <maker2413@gmail.com>
2024-09-30 21:33:47 -0500

    made my way back through the hello world section

--- helloWorld/hello.go
+++ helloWorld/hello.go
@@ -2,10 +2,48 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name + "!"
+// Constants can be declared in a block
+const (
+	spanish  = "Spanish"
+	french   = "French"
+	japanese = "Japanese"
+
+	englishHelloPrefix  = "Hello, "
+	spanishHelloPrefix  = "Hola, "
+	frenchHelloPrefix   = "Bonjour, "
+	japaneseHelloPrefix = "Konnichiwa, "
+)
+
+// In golang public functions begin with a capital letter and private functions
+// start with a lowercase letter.
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(language) + name + "!"
+}
+
+// prefix in this case is a named return value.
+// This creates a variable called prefix in our function.
+// By default this variable will default to "zero" value (ints are 0 and strings are "").
+// You can return the value of this variable at any point by just calling return
+// Also note that this function begins with a lowercase letter so it is private.
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case japanese:
+		prefix = japaneseHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,10 +3,39 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Ethan")
-	want := "Hello, Ethan!"
+	//t.Run is how we can define subtests, which allow us to group together tests in a func
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Ethan", "")
+		want := "Hello, Ethan!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello in Spanish to people", func(t *testing.T) {
+		got := Hello("Ethan", "Spanish")
+		want := "Hola, Ethan!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello in French to people", func(t *testing.T) {
+		got := Hello("Ethan", "French")
+		want := "Bonjour, Ethan!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello in Japanese to people", func(t *testing.T) {
+		got := Hello("Ethan", "Japanese")
+		want := "Konnichiwa, Ethan!"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+// testing.TB is an interface that accepts tests and benchmarks
+func assertCorrectMessage(t testing.TB, got, want string) {
+	//t.Helper will make the failure message show the line number test that called this function
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got: %q, want: %q", got, want)
 	}
 }
