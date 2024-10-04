3dc792a763d1c7f5085aba4487fedda50ca4196b
maker2413 <maker2413@gmail.com>
2024-10-04 01:02:27 -0500

    worked through chapter on pointers

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,24 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-type Wallet struct {
-	balance float64
+type Bitcoin float64
+
+// This is a Stringer interface. Stringer is defined in the fmt package and
+// lets you define how your type is printed when used with the %s format string
+// in prints.
+func (b Bitcoin) String() string {
+	// Sprintf allows us to format the String we are returning.
+	return fmt.Sprintf("%.2f BTC", b)
 }
 
-func (w Wallet) Deposit(amount float64) {
-	fmt.Printf("address of balance in Deposit is: %p \n", &w.balance)
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// (*w).balance += amount is also valid, but is optional in Go
+	// unlike other languages.
 	w.balance += amount
 }
 
-func (w Wallet) Balance() float64 {
+// This var is declared globally so it can be used in our tests as well.
+var ErrInsufficientFunds = errors.New("Error: attempting to withdraw more than balance!")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	} else {
+		w.balance -= amount
+		return nil
+	}
+}
+
+// If we don't do w *Wallet like the deposit function then we aren't using a pointer
+// to access our original object, but instead we will be given a copy of our struct.
+// Since our Balance function just needs to return the balance a copy of our struct
+// will work. In general it is best practice to interact with the original though.
+func (w Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
 func main() {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
-	fmt.Printf("Balance: %.2f", wallet.Balance())
+	fmt.Printf("Balance: %s", wallet.Balance())
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,14 +3,60 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
-	wallet.Deposit(10)
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-	want := 10.00
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	assertNoError := func(t testing.TB, got error) {
+		t.Helper()
+
+		// nil is synonymous with null from other programming languages.
+		if got != nil {
+			// t.Fatal will stop all tests and print its message if called.
+			t.Fatal("Didn't want an error, but got one.")
+		}
+	}
+
+	assertError := func(t testing.TB, got error) {
+		t.Helper()
+
+		if got == nil {
+			t.Fatal("Wanted an error, but didn't get one.")
+		}
+		// got.Error() gives us the string contents of the error message.
+		if got.Error() != ErrInsufficientFunds.Error() {
+			t.Errorf("got %s want %s", got, ErrInsufficientFunds)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+		assertNoError(t, err)
+	})
+	t.Run("Withdraw insufficent funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(40))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
+	})
 }
