d6615969d109eec07a10f18ba75f93ec4960b93f
maker2413 <maker2413@gmail.com>
2024-10-06 12:22:04 -0500

    finished dependency injection chapter

--- dependencyInjection/di.go
+++ dependencyInjection/di.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(name string) {
-	fmt.Printf("Hello, %s", name)
+// By using io.Writer we are still able to use this function in main by passing in os.Stdout.
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Ethan")
 }
