8907428251fc4ce57791e8784846506444c42ea7
maker2413 <maker2413@gmail.com>
2024-10-06 15:56:19 -0500

    finished the mocking chapter

---
+++ mocking/countdown.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+const write = "write"
+const sleep = "sleep"
+
+type Sleeper interface {
+	Sleep()
+}
+
+// Spies are a kind of mock which can record how a dependency is used. They can
+// record the arguments sent in, how many times it has been called, etc. In our
+// case, we're keeping track of how many times Sleep() is called so we can check
+// it in our test.
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+type ConfigurableSleeper struct {
+	// used to configure the time slept
+	duration time.Duration
+
+	// used as a way to pass in a sleep function
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
