b6d47341f571875bd49ba8579ecb6b0e09e98a2f
maker2413 <maker2413@gmail.com>
2024-10-23 22:17:58 -0500

    finished last chapter!

--- revisitingArrays/collection_fun.go
+++ revisitingArrays/collection_fun.go
@@ -2,10 +2,19 @@ package main
 
 // Reduce captures the essence of the pattern, it's a function that takes a collection,
 // an accumulating function, an initial value, and returns a single value.
-func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
 		result = f(result, x)
 	}
 	return result
+}
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
 }
