76ee4d873ca9a6073ab60941d73909e5a475b48b
maker2413 <maker2413@gmail.com>
2024-11-17 16:24:09 -0600

    got first revision of http server working

---
+++ httpServer/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(name string) string {
+	switch name {
+	case "Pepper":
+		return "20"
+	case "Floyd":
+		return "10"
+	}
+
+	return ""
+}
