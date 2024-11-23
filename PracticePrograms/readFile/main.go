b353ee0629263fdfbcc8a5a2e30e19d7b44c93eb
maker2413 <maker2413@gmail.com>
2024-11-23 11:28:45 -0600

    fixing fatal error in readFile

--- PracticePrograms/readFile/main.go
+++ PracticePrograms/readFile/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
@@ -10,16 +11,14 @@ const fileError string = "Please provide a valid file name to read from!"
 
 func main() {
 	if len(os.Args) <= 1 {
-		fmt.Println(fileError)
-		os.Exit(1)
+		log.Fatal(fileError)
 	}
 
 	fmt.Printf("Opening: %s\n", os.Args[1])
 	f, err := os.Open(os.Args[1])
 
 	if err != nil {
-		fmt.Println(fileError)
-		os.Exit(1)
+		log.Fatal(fileError)
 	}
 
 	io.Copy(os.Stdout, f)
