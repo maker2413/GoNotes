e4e0618ce2c4a6cd6950dada377c0ed0c50d1b2a
maker2413 <maker2413@gmail.com>
2024-12-09 21:38:28 -0600

    testing out go test coverprofile

--- PracticePrograms/primeNumbers/main.go
+++ PracticePrograms/primeNumbers/main.go
@@ -13,19 +13,19 @@ func main() {
 func isPrime(n int) (bool, string) {
 	// 0 and 1 are not prime by definition
 	if n == 0 || n == 1 {
-		return false, fmt.Sprintf("%d is not prime by definition", n)
+		return false, fmt.Sprintf("%d is not prime, by definition!", n)
 	}
 
 	// negative numbers are not prime
 	if n < 0 {
-		return false, "Negative numbers are not prime, by definition"
+		return false, "Negative numbers are not prime, by definition!"
 	}
 
 	// use the modulus operator repeatedly to see if we have a prime number
 	for i := 2; i <= n/2; i++ {
 		if n%i == 0 {
 			// not a prime number
-			return false, fmt.Sprintf("%d is not a prime number because it is divisible by %d", n, i)
+			return false, fmt.Sprintf("%d is not a prime number because it is divisible by %d!", n, i)
 		}
 	}
 
