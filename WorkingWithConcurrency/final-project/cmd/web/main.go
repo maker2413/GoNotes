4befaae6d31553c8297f38f047aa2e654d8b9a4d
maker2413 <maker2413@gmail.com>
2024-12-14 13:49:21 -0600

    finished sleeping barber problem and created skeleton for final project

---
+++ WorkingWithConcurrency/final-project/cmd/web/main.go
@@ -0,0 +1,19 @@
+package main
+
+const webPort = "80"
+
+func main() {
+	// Connect to database
+
+	// Create sessions
+
+	// Create some channels
+
+	// Create waitgroup
+
+	// Set up the application config
+
+	// Set up mail
+
+	// Listen for web connections
+}
--- WorkingWithConcurrency/sleeping-barber/main.go
+++ WorkingWithConcurrency/sleeping-barber/main.go
@@ -26,22 +26,80 @@
 
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
+)
+
 // variables
+var seatingCapacity = 10
+var arrivalRate = 100 // Miliseconds
+var cutDuration = 1000 * time.Millisecond
+var timeOpen = 10 * time.Second
 
 func main() {
 	// seed random number generator
+	rand.Seed(time.Now().UnixNano())
 
 	// print a welcome message
+	color.Yellow("The Sleeping Barber Problem")
+	color.Yellow("---------------------------")
 
 	// create channels if we need any
+	clientChan := make(chan string, seatingCapacity)
+	doneChan := make(chan bool)
 
 	// create the barbershop
+	shop := BarberShop{
+		ShopCapacity:    seatingCapacity,
+		HairCutDuration: cutDuration,
+		NumberOfBarbers: 0,
+		ClientsChan:     clientChan,
+		BarbersDoneChan: doneChan,
+		Open:            true,
+	}
 
-	// add barbers
+	color.Green("The shop is open for the day!")
 
-	// start the barbershop as a goroutine
+	// Add barbers
+	shop.addBarber("Frank")
+	shop.addBarber("Gerard")
+	shop.addBarber("Milton")
+	shop.addBarber("Susan")
+	shop.addBarber("Kelly")
+	shop.addBarber("Pat")
 
-	// add clients
+	// Start the barbershop as a goroutine
+	shopClosing := make(chan bool)
+	closed := make(chan bool)
 
-	// block until the barbershop is closed
+	go func() {
+		<-time.After(timeOpen)
+		shopClosing <- true
+		shop.closeShopForDay()
+		closed <- true
+	}()
+
+	// Add clients
+	i := 1
+
+	go func() {
+		for {
+			// get a random number with average arrival rate
+			randomMilliseconds := rand.Int() % (2 * arrivalRate)
+			select {
+			case <-shopClosing:
+				return
+			case <-time.After(time.Millisecond * time.Duration(randomMilliseconds)):
+				shop.addClient(fmt.Sprintf("Client #%d", i))
+				i++
+			}
+		}
+	}()
+
+	// Block until the barbershop is closed
+	<-closed
 }
