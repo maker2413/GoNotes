e13b1e15b38c489695a7b315a08d410530ed6f34
maker2413 <maker2413@gmail.com>
2024-12-29 11:52:27 -0600

    added downloading static files functionality

---
+++ BuildingGoModules/app-download/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/maker2413/GoNotes/BuildingGoModules/toolkit"
+)
+
+func main() {
+	// Get routes
+	mux := routes()
+
+	// Start a server
+	log.Println("Server Started on port 8080...")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/download", downloadFile)
+
+	return mux
+}
+
+func downloadFile(w http.ResponseWriter, r *http.Request) {
+	t := toolkit.Tools{}
+	t.DownloadStaticFile(w, r, "./files", "pic.jpg", "puppy.jpg")
+}
