d8bf787d5ed1ad8e19170a9b68e615ca9931a5cf
maker2413 <maker2413@gmail.com>
2024-12-29 10:31:49 -0600

    added functionality to create dir if it doesn't exist

---
+++ BuildingGoModules/app-dir/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/maker2413/GoNotes/BuildingGoModules/toolkit"
+
+func main() {
+	var tools toolkit.Tools
+
+	tools.CreateDirIfNotExist("./test-dir")
+}
--- BuildingGoModules/toolkit/tools.go
+++ BuildingGoModules/toolkit/tools.go
@@ -42,6 +42,8 @@ type UploadedFile struct {
 	FileSize         int64
 }
 
+// UploadOneFile is just a convenience method that calls UploadFiles, but expects only one
+// file to be in the upload.
 func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
@@ -56,6 +58,10 @@ func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool)
 	return files[0], nil
 }
 
+// UploadFiles uploads one or more file to a specified directory, and gives the files a
+// random name. It returns a slice containing the newly named files, the original file
+// names, the size of the files, and potentially an error. If the optional last parameter
+// is set to true, then we will not rename the files, but will use the original file names.
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
@@ -68,7 +74,12 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 		t.MaxFileSize = 1024 * 1024 * 1024
 	}
 
-	err := r.ParseMultipartForm(int64(t.MaxFileSize))
+	err := t.CreateDirIfNotExist(uploadDir)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.ParseMultipartForm(int64(t.MaxFileSize))
 	if err != nil {
 		return nil, errors.New("The uploaded file is too big")
 	}
@@ -142,4 +153,17 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 		}
 	}
 	return uploadedFiles, nil
+}
+
+// CreateDirIfNotExist creates a directory, and all necessary parents, if it does not exist
+func (t *Tools) CreateDirIfNotExist(path string) error {
+	const mode = 0755
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
