aa0cf494db107bd52dcc2bfbcdeda808839c79b5
maker2413 <maker2413@gmail.com>
2025-01-06 21:05:41 -0600

    finished creating streamer application to showcase worker pool design pattern

--- DesignPatternsInGo/workerPool/wp/app/main.go
+++ DesignPatternsInGo/workerPool/wp/app/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// Define number of workers and jobs
 	const numJobs = 4
-	const numWorkers = 1
+	const numWorkers = 4
 
 	// Create channels for work and results
 	notifyChan := make(chan streamer.ProcessingMessage, numJobs)
@@ -19,16 +19,42 @@ func main() {
 
 	// Get a worker pool.
 	wp := streamer.New(videoQueue, numWorkers)
-	fmt.Println("wp:", wp)
 
 	// Start the worker pool.
 	wp.Run()
+	fmt.Println("Worker pool started. Press enter to continue.")
+	_, _ = fmt.Scanln()
 
-	// Create a video to send to the worker pool.
-	video := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
+	// Create a video that converts mp4 to web ready format.
+	video1 := wp.NewVideo(1, "./input/puppy1.mp4", "./output", "mp4", notifyChan, nil)
+
+	// Create a second video that should fail.
+	video2 := wp.NewVideo(2, "./input/bad.txt", "./output", "mp4", notifyChan, nil)
+
+	// Create a third video that converts mp4 to hls
+	ops := &streamer.VideoOptions{
+		RenameOutput:    true,
+		SegmentDuration: 10,
+		MaxRate1080p:    "1200k",
+		MaxRate720p:     "600k",
+		MaxRate480p:     "400k",
+	}
+	video3 := wp.NewVideo(3, "./input/puppy2.mp4", "./output", "hls", notifyChan, ops)
+
+	// Create a video that converts mp4 to web ready format.
+	video4 := wp.NewVideo(4, "./input/puppy2.mp4", "./output", "mp4", notifyChan, nil)
 
 	// Send the videos to the worker pool.
-	videoQueue <- streamer.VideoProcessingJob{Video: video}
+	videoQueue <- streamer.VideoProcessingJob{Video: video1}
+	videoQueue <- streamer.VideoProcessingJob{Video: video2}
+	videoQueue <- streamer.VideoProcessingJob{Video: video3}
+	videoQueue <- streamer.VideoProcessingJob{Video: video4}
 
 	// Print out results.
+	for i := 1; i <= numJobs; i++ {
+		msg := <-notifyChan
+		fmt.Println("i:", i, "msg:", msg)
+	}
+
+	fmt.Println("Done")
 }
