8fe1019692b699a66055ff45a05ab2e971de8d62
maker2413 <maker2413@gmail.com>
2025-01-09 14:27:01 -0600

    added grpc support to microservices project

--- Microservices/logger-service/cmd/api/main.go
+++ Microservices/logger-service/cmd/api/main.go
@@ -54,6 +54,8 @@ func main() {
 	err = rpc.Register(new(RPCServer))
 	go app.rpcListen()
 
+	go app.gRPCListen()
+
 	// Start web server
 	//go app.serve()
 	log.Println("Starting service on port", webPort)
